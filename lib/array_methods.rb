3082b54587fdf8614a94a159d1883060c760915a
Paul Leugemors <pleugemors@yahoo.com>
2019-10-16 04:53:05 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,12 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  counter = 0 
+  while array[counter]{
+    if array[counter]== value_to_find
+      return counter
+    end
+    counter += 1
+  }
+  return nil
 end
 
 def find_max_value(array)
